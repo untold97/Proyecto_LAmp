ba067b2dc982ee8128183e2dbd9bc901ab579dc7
untold97 <321030090@upjr.edu.mx>
2025-06-11 08:43:39 -0600

    agrege gitignore

---
+++ base_datos/patito.sql
@@ -0,0 +1,11 @@
+CREATE TABLE estudiantes(
+    id int(11) NOT NULL,
+    nombre varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+INSERT INTO estudiantes(id, nombre) VALUES (435, 'Juan Ramirez');
+INSERT INTO estudiantes(id, nombre) VALUES (552, 'Ana Perez');
+INSERT INTO estudiantes(id, nombre) VALUES (125, 'Miguel Torrez');
+INSERT INTO estudiantes(id, nombre) VALUES (999, 'Sofia Pereira');
+INSERT INTO estudiantes(id, nombre) VALUES (745, 'Paloma Vega');
+INSERT INTO estudiantes(id, nombre) VALUES (745, 'Miriam Rangel');
+INSERT INTO estudiantes(id, nombre) VALUES (741, 'Jesus Garcia');
